734e4ff031a058a27df44a3e0ff70d55d910fbc0
jaeyoungjang2 <lion85488@gmail.com>
2023-02-19 21:06:34 +0900

    feat : first commit

---
+++ src/main/java/com/example/tobyboot/TobybootApplication.java
@@ -0,0 +1,13 @@
+package com.example.tobyboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobybootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobybootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tobyboot/TobybootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobyboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobybootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
