d4c42e2e8995269c7d6c9991bca74baac59e984d
jaeyoungjang2 <lion85488@gmail.com>
2023-02-20 20:08:29 +0900

    feat : front controller url == hello, method == get 인 경우

--- src/main/java/com/example/tobyboot/HelloController.java
+++ src/main/java/com/example/tobyboot/HelloController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 //    http://localhost:8080/hello?name=string
-    @GetMapping("/hello")
-    public String hello(String name) {
-        return "Hello " + name;
-    }
+//    @GetMapping("/hello")
+//    public String hello(String name) {
+//        return "Hello " + name;
+//    }
 }
--- src/main/java/com/example/tobyboot/TobybootApplication.java
+++ src/main/java/com/example/tobyboot/TobybootApplication.java
@@ -3,6 +3,7 @@ package com.example.tobyboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -19,22 +20,32 @@ public class TobybootApplication {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         // TomcatServletWebServerFactory serverFactory = new JettyServletWebServerFactory();
 
-        // hello로 들어오는 요청은 hello servlet이 처리하도록 한다는 것이다.
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello2", new HttpServlet() {
+            servletContext.addServlet("frontcontrller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    String name = req.getParameter("name");
+                    // 인증, 보안, 다국어, 공통으로 처리해야 하는 기능이 구현되어 있다고 하자
 
-                    // 응답에 관한 코드
-                    // 상태 라인, 상태 코드
-                    resp.setStatus(HttpStatus.OK.value());
-                    // 컨텐츠 타입 헤더
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    // 바디
-                    resp.getWriter().println("Hello " + name);
+                    // uri == hello, method == get
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
+
+                        // 응답에 관한 코드
+                        // 상태 라인, 상태 코드
+                        resp.setStatus(HttpStatus.OK.value());
+                        // 컨텐츠 타입 헤더
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        // 바디
+                        resp.getWriter().println("Hello " + name);
+                    } else if (req.getRequestURI().equals("/user")) {
+                        //
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+
                 }
-            }).addMapping("/hello2");
+//                / 밑으로 들어오는 모든 요청을 처리하겠다. (front controller 책임을 맡게 된다.)
+            }).addMapping("/*");
         });
         // 톰캣 서블릿 컨테이너가 동작한다.
         webServer.start();
