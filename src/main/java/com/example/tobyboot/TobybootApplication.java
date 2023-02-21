02a737898cee387b9c5661946d98efbde5074ab0
jaeyoungjang2 <lion85488@gmail.com>
2023-02-21 20:51:12 +0900

    feat: 자바코드 구성 정보 사용  - bean 등록  - 의존 관계 등록  - AnnotationConfigWebApplicationContext 활용

--- src/main/java/com/example/tobyboot/TobybootApplication.java
+++ src/main/java/com/example/tobyboot/TobybootApplication.java
@@ -1,19 +1,32 @@
 package com.example.tobyboot;
 
 import com.example.tobyboot.toby.HelloController;
+import com.example.tobyboot.toby.HelloService;
 import com.example.tobyboot.toby.SimpleHelloService;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-
+@Configuration
 public class TobybootApplication {
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
         // spring container 만드는 작업
         // 익명 클래스
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 // 생략 하면 안됨.
@@ -23,16 +36,15 @@ public class TobybootApplication {
                 // servlet container를 코드로 실행하면서 servlet을 등록하는 작업
                 WebServer webServer = serverFactory.getWebServer(servletContext -> {
                     servletContext.addServlet("dispatcherServlet",
-                            new DispatcherServlet(this)
-                    ).addMapping("/*");
+                            new DispatcherServlet(this))
+                    .addMapping("/*");
                 });
                 // 톰캣 서블릿 컨테이너가 동작한다.
                 webServer.start();
             }
         };
         // bean 등록
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.register(TobybootApplication.class);
         // 초기화 작업
         applicationContext.refresh();
 
