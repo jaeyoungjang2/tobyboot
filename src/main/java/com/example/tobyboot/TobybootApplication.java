ffe1a680144ef0ce1da6aaf2a01908806ad2985e
jaeyoungjang2 <lion85488@gmail.com>
2023-02-21 20:29:59 +0900

    feat : spring container 초기화 할 때, servlet container도 초기화 하도록 코드 수정 servlet container를 생성하고 servlet 초기화 작업은 spring container 초기화 되는 과정에 일어나도록 코드를 수정할 예정 spring container 초기화 작업은 applicationcontext.refresh()에서 시작된다.

--- src/main/java/com/example/tobyboot/TobybootApplication.java
+++ src/main/java/com/example/tobyboot/TobybootApplication.java
@@ -11,19 +11,30 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class TobybootApplication {
 
     public static void main(String[] args) {
-        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        // spring container 만드는 작업
+        // 익명 클래스
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                // 생략 하면 안됨.
+                super.onRefresh();
+                // 서블릿 컨테이너를 초기화하는 코드
+                TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                // servlet container를 코드로 실행하면서 servlet을 등록하는 작업
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this)
+                    ).addMapping("/*");
+                });
+                // 톰캣 서블릿 컨테이너가 동작한다.
+                webServer.start();
+            }
+        };
+        // bean 등록
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
+        // 초기화 작업
         applicationContext.refresh();
-
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext)
-            ).addMapping("/*");
-        });
-        // 톰캣 서블릿 컨테이너가 동작한다.
-        webServer.start();
 
         System.out.println("Hello containerless Standalone Application");
     }
--- src/main/java/com/example/tobyboot/toby/HelloController.java
+++ src/main/java/com/example/tobyboot/toby/HelloController.java
@@ -1,10 +1,12 @@
 package com.example.tobyboot.toby;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Objects;
 
-@RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
@@ -13,6 +15,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
