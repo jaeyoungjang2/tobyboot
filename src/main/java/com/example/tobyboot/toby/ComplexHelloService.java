e566247de7c144a577a1b89d2f8f1d1417c60021
jaeyoungjang2 <lion85488@gmail.com>
2023-02-22 22:27:01 +0900

    refactor: 필요없는 코드 삭제

--- src/main/java/com/example/tobyboot/toby/ComplexHelloService.java
+++
@@ -1,8 +0,0 @@
-package com.example.tobyboot.toby;
-
-public class ComplexHelloService implements HelloService {
-    @Override
-    public String sayHello(String name) {
-        return "bye " + name;
-    }
-}
