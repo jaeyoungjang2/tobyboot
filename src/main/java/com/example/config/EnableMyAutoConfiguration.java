213be43acc9690999c2645326189220653ee83cd
jaeyoungjang2 <lion85488@gmail.com>
2023-02-23 21:13:50 +0900

    feat: 동적인 자동 구성 정보 등록 https://daram.tistory.com/520

--- src/main/java/com/example/config/EnableMyAutoConfiguration.java
+++ src/main/java/com/example/config/EnableMyAutoConfiguration.java
@@ -1,7 +1,5 @@
 package com.example.config;
 
-import com.example.config.autoconfig.DispatcherServletConfig;
-import com.example.config.autoconfig.TomcatWebServerConfig;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.ElementType;
@@ -12,6 +10,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 // type : class, interface, enum을 대상으로 부여
 @Target(ElementType.TYPE)
-@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
+@Import(MyAutoConfigImportSelector.class)
 public @interface EnableMyAutoConfiguration {
 }
