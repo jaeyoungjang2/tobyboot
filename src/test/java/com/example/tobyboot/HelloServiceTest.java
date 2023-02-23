a4b6f999177c1211d0a5241dd30724ff49dcf269
jaeyoungjang2 <lion85488@gmail.com>
2023-02-23 19:17:35 +0900

    feat: meta annotation , compose annotation

--- src/test/java/com/example/tobyboot/HelloServiceTest.java
+++ src/test/java/com/example/tobyboot/HelloServiceTest.java
@@ -5,6 +5,26 @@ import com.example.tobyboot.toby.SimpleHelloService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest {
+
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+// annotation type으로 지정을 하였기 때문에 위에 meta annotation을 만들 때 또 사용할 수 있다.
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Test
+@interface UnitTest {
+
+}
+
 public class HelloServiceTest {
 
     @Test
